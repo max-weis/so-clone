28c7704383ffbcc994efd5e442e0316f725d9908
Max Weis <maxweis@protonmail.com>
2020-03-22 21:52:34 +0100

    fix typo

--- qa/src/test/java/main/de/maxwell/qa/question/domain/QuestionNotFoundExceptionTest.java
+++ qa/src/test/java/main/de/maxwell/qa/question/domain/QuestionNotFoundExceptionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class QuestionNotFoundExcpetionTest {
+public class QuestionNotFoundExceptionTest {
 
     @Test
     public void testException() {

778e53c29bf676df759e229d2a8326c77b9c60d5
Max Weis <maxweis@protonmail.com>
2020-04-13 16:17:04 +0200

    add sidenav

---
+++ portal/src/app/home/second-nav/second-nav.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecondNavComponent } from './second-nav.component';
+
+describe('SecondNavComponent', () => {
+  let component: SecondNavComponent;
+  let fixture: ComponentFixture<SecondNavComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SecondNavComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SecondNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ portal/src/app/home/second-nav/second-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-nav',
+  templateUrl: './second-nav.component.html',
+  styleUrls: ['./second-nav.component.scss']
+})
+export class SecondNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
